722fbae64f0b699ecf8fd0ce8e2b3f67937b99ba
HP <HP@BOTNIMOUS>
2022-11-01 23:55:10 +0530

    Add project files.

---
+++ temprature/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TemperatureConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Fahrenheit temperature : ");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("The converted Fahrenheit to Celsius temperature is : " + celsius);
+            Console.WriteLine("\n");
+            Console.Write("Enter Celsius temperature : ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("The converted Celsius to Fahrenheit temperature is : " + fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
